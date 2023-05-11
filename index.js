3fb7d2f3cc6497495e896641a5d9435b1ff95f5f
bangpham <uzumakinarutoshin@gmail.com>
2023-05-11 21:16:44 +0700

    fix register 1

--- index.js
+++ index.js
@@ -55,24 +55,24 @@ app.use(function (req, res, next) {
 
 db.connect();
 // Curb Cores Error by adding a header here
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader(
-//         'Access-Control-Allow-Headers',
-//         'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
-//     );
-//     res.setHeader(
-//         'Access-Control-Allow-Methods',
-//         'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-//     );
-//     next();
-// });
-
 app.use((req, res, next) => {
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Origin', 'https://szhin.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    );
     next();
 });
+
+// app.use((req, res, next) => {
+//     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.setHeader('Access-Control-Allow-Origin', 'https://szhin.vercel.app');
+//     next();
+// });
 route(app);
 
 app.listen(port, () => {
